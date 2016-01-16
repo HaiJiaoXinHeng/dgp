752aa592b683e873133f0afe73fa2d108a7ffd1f
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-01-15 18:21:07 -0800

    [brief] Adds code solutions for lab 1.
    [detailed]

--- lab01_eigen/eigen.cpp
+++ lab01_eigen/eigen.cpp
@@ -8,23 +8,58 @@ int main(){
     // http://eigen.tuxfamily.org/dox/AsciiQuickReference.txt
     
     // Quick example: take two vectors, fill them, and do something.
-    
+    Eigen::Vector3f u, v;
+    u(0) = 1.0;
+    u(1) = 0.0;
+    u(2) = 0.0;
+
     // Another way of initializing a vector/matrix
-    
+    v << 0.0, 1.0, 0.0;
+
+    auto wStar = u + v;
+
+    mDebug() << "wStar: " << wStar.transpose();
 
     // Display them to standard output
-       
+    mDebug() << "u: " << u.transpose();
+    mDebug() << "v: " << v.transpose();
+
+    auto w = u.cross(v);
+    mDebug() << u.transpose() << " dot " << v.transpose() << " = " << w.transpose();
+    double dot = u.dot(v);
+    mDebug() << u.transpose() << " dot " << v.transpose() << " = " << dot;
+    
 
     // Notice that these two now have the same value. Eigen normalizes in place.
     // Note: be careful in performing A=A.something()! See "ALIASING" topic in Eigen
-    
+    auto uHat = u.normalized();
+    u.normalize();
+    auto normU = u.norm();
+    auto normUHat = uHat.norm();
+    mDebug() << "\n";
+    mDebug() << "normU:" << normU;
+    mDebug() << "normUHat: " << normUHat;
+
     // Now let's set up a matrix.
-    
+    Eigen::Matrix2f m;
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << "\nMatrix m:\n" << m << std::endl; 
+
     // Again, inline initialization
-    
+    Eigen::Matrix3f n;
+    n << 1, 2, 3,
+        4, 5, 6,
+        7, 8, 9;
+    std::cout << "\nMatrix n:\n" << n << std::endl; 
 
     // And we can do the operations seen on the slides.
     //auto m1 = m + n; // This will fail.
-        
+    auto m2 = Eigen::Matrix2f::Identity();
+    auto m3 = m + m2; // This will not.
+    std::cout << "\nMatrix m3:\n" << m3 << std::endl; 
+    
     return 0;
 }
--- lab01_triangle/triangle.cpp
+++ lab01_triangle/triangle.cpp
@@ -4,7 +4,40 @@
 using namespace OpenGP;
 
 int main(){
+    SurfaceMesh mesh;
     
+    SurfaceMesh::Vertex v1 = mesh.add_vertex(Vec3(0, 0, 0));
+    SurfaceMesh::Vertex v2 = mesh.add_vertex(Vec3(1, 0, 0));
+    SurfaceMesh::Vertex v3 = mesh.add_vertex(Vec3(0, 1, 0));
+    mesh.add_triangle(v1, v2, v3);
+
+    mesh.update_face_normals();
+
+    // Now let's scale a single vertex.
+    Point pos = mesh.position(v2);
+    mesh.position(v2) = pos * 2;
+
+    mesh.position(v2) = pos / 2;
+
+    // Now let's scale the whole mesh.
+    SurfaceMesh::Vertex_iterator vIt, vBegin, vEnd;
+    vBegin = mesh.vertices_begin();
+    vEnd = mesh.vertices_end();
+    for (vIt = vBegin; vIt != vEnd; ++vIt){
+        SurfaceMesh::Vertex v = *vIt;
+        // And now we can scale it as before.
+        Point pos = mesh.position(v);
+        mesh.position(v) = pos * 2;
+    }
+
+    // A better way.
+    for(const auto& vertex : mesh.vertices()){
+        Point ps = mesh.position(vertex);
+        mesh.position(vertex) = ps / 2;
+    }
+
     GlfwWindow window("Tetrahedra", 400, 400);
+    SurfaceMeshRenderFlat render(mesh);
+    window.scene.add(render);
     window.run();
 }
