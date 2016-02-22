0d7e62c9f0a9492dcd801ece7d42db6cba25902a
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-02-22 14:02:15 -0800

    [brief] Adds solutions for Smoothing lab.
    [detailed]

--- lab05_smoothing/Smoother.cpp
+++ lab05_smoothing/Smoother.cpp
@@ -21,26 +21,81 @@ void Smoother::use_cotan_laplacian()
     unsigned int n = mesh.n_vertices();
     typedef Eigen::Triplet<Scalar> Triplet;
 
-    std::vector<Triplet> tripletList;
-    tripletList.reserve(n * n);
+    SurfaceMesh::Halfedge betaEdge, alphaEdge;
+    SurfaceMesh::Vertex vBeta, vAlpha, v_j;
+    Point p_i, p_j, p_b, p_a, d_ib, d_ia, d_aj, d_bj, d_ij;
+    Scalar alpha, beta, area, cotanAlpha, cotanBeta, cotanSum;
 
-    // TODO: Fill in the code to compute the cotan laplacian matrix.
-    // \li First, compute the cotans of the corresponding angles and the area.
-    // \li Second, store them in the appropriate entries of the matrices D and M.
-    // \li Last, multiply D * M to get the laplacian.
+    SparseMatrix<Scalar> M(n, n);
+    SparseMatrix<Scalar> D(n, n);
+
+    std::vector<Triplet> mList;
+    std::vector<Triplet> diagonalList;
+
+    mList.reserve(n * n);
+    diagonalList.reserve(n * n);
 
     for (auto const& v_i : mesh.vertices())
     {
+        cotanSum = 0.0f;
+        area = 0.0f;
+
         for (auto const& edge : mesh.halfedges(v_i))
         {
-            auto v_j = mesh.to_vertex(edge);
-            tripletList.push_back(Triplet(v_i.idx(), v_j.idx(), 0));
+            // Grab the vertex that the current edge points to.
+            v_j = mesh.to_vertex(edge);
+
+            // Now grab the edges that point to the two vertices
+            // that define alpha and beta.
+            betaEdge = mesh.next_halfedge(edge);
+            alphaEdge = mesh.next_halfedge(mesh.opposite_halfedge(edge));
+
+            // Grab the corresponding vertices.
+            vBeta = mesh.to_vertex(betaEdge);
+            vAlpha = mesh.to_vertex(alphaEdge);
+
+            // Now get the points to compute the angles.
+            p_i = mesh.position(v_i);
+            p_j = mesh.position(v_j);
+            p_b = mesh.position(vBeta);
+            p_a = mesh.position(vAlpha);
+
+            // Set the vectors.
+            d_ib = (p_b - p_i).normalized();
+            d_ia = (p_a - p_i).normalized();
+            d_aj = (p_j - p_a).normalized();
+            d_bj = (p_j - p_b).normalized();
+            d_ij = (p_j - p_i).normalized();
+
+            // Compute the angles.
+            beta = std::acos(d_ib.dot(d_bj));
+            alpha = std::acos(d_ia.dot(d_aj));
+
+            // Compute their cotangents.
+            cotanAlpha = 1.0f / std::tan(alpha);
+            cotanBeta = 1.0f / std::tan(beta);
+
+            // Set the non-diagonal entry.
+            mList.push_back(Triplet(v_i.idx(), v_j.idx(), cotanAlpha + cotanBeta));
+            cotanSum += cotanAlpha + cotanBeta;
+
+            // Compute the area.
+            area += (1 / 6.0f) * (d_ij.cross(d_ia)).norm();
         }
 
-        tripletList.push_back(Triplet(v_i.idx(), v_i.idx(), 1));
+        // Store the negative sum of the cotans in the diagonal.
+        mList.push_back(Triplet(v_i.idx(), v_i.idx(), -cotanSum));
+
+        // Store one half of the total area on the diagonal matrix.
+        diagonalList.push_back(Triplet(v_i.idx(), v_i.idx(), 0.5f * area));
+
     }
 
-    L.setFromTriplets(tripletList.begin(), tripletList.end());
+    // Compute the final Laplacian and return it.
+    M.setFromTriplets(mList.begin(), mList.end());
+    D.setFromTriplets(diagonalList.begin(), diagonalList.end());
+
+    L = D * M;
 }
 
 void Smoother::use_graph_laplacian()
@@ -50,26 +105,29 @@ void Smoother::use_graph_laplacian()
 
     typedef Eigen::Triplet<Scalar> Triplet;
 
-    std::vector<Triplet> tripletList;
-    tripletList.reserve(n * n);
+    std::vector<Triplet> laplacianList;
+    laplacianList.reserve(n * n);
 
-    // TODO: Fill in the code to compute the graph laplacian matrix.
-    // \li First, set the adjacency values correspodning to each vertex in the one-ring.
-    // \li Next, compute the degree (valence) as you go along.
-    // \li Last, store the degree and adjacency entries in the matrix and generate the laplacian.
-
+    int degree;
     for (auto const& v_i : mesh.vertices())
     {
+        degree = 0;
         for (auto const& edge : mesh.halfedges(v_i))
         {
             auto v_j = mesh.to_vertex(edge);
-            tripletList.push_back(Triplet(v_i.idx(), v_j.idx(), 0));
+            
+            // Insert -1 on the corresponding entry in the matrix.
+            laplacianList.push_back(Triplet(v_i.idx(), v_j.idx(), -1));
+
+            // Increase the degree of the v_i.
+            degree++;
         }
 
-        tripletList.push_back(Triplet(v_i.idx(), v_i.idx(), 1));
+        // Now insert the degree of the v_i into the diagonal.
+        laplacianList.push_back(Triplet(v_i.idx(), v_i.idx(), degree));
     }
 
-    L.setFromTriplets(tripletList.begin(), tripletList.end());
+    L.setFromTriplets(laplacianList.begin(), laplacianList.end());
 }
 
 void Smoother::smooth_explicit(OpenGP::Scalar lambda)
@@ -94,13 +152,14 @@ void Smoother::smooth_explicit(OpenGP::Scalar lambda)
     P_t.transposeInPlace();
     P_t1.transposeInPlace();
 
-    // TODO: Fill in the code for explicit smoothing.
+    P_t1 = (I - lambda * L) * P_t;
 
+    // Now assign the points back into the mesh.
     i = 0;
     for (auto const& vertex : mesh.vertices())
     {
-        mesh.position(vertex) = P_t.row(i);
-        ++i;
+        mesh.position(vertex) = P_t1.row(i);
+        i++;
     }
 }
 
@@ -124,12 +183,15 @@ void Smoother::smooth_implicit(OpenGP::Scalar lambda)
     P_t.transposeInPlace();
     P_t1.transposeInPlace();
 
-    // TODO: Fill in the code for implicit smoothing.
+    SparseMatrix<Scalar> A = (I - lambda * L);
+    solve_linear_least_square(A, P_t, P_t1);
+    P_t = P_t1;
 
+    // Now assign the points back into the mesh.
     i = 0;
     for (auto const& vertex : mesh.vertices())
     {
-        mesh.position(vertex) = P_t.row(i);
+        mesh.position(vertex) = P_t1.row(i);
         ++i;
     }
 }
